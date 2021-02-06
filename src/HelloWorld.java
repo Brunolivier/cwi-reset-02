f50518427a1b417ff40dc66f0859f2b4b60521df
Brunolivier <bpereiraoliveira@gmail.com>
2021-02-06 14:44:16 -0300

    Commit do Hello World! A maldição está quebrada.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Ola mundo!");
+        System.out.println("Ola mundo! A maldição está quebrada.");
     }
 
 }
