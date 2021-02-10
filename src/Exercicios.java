6f0dcb84c53360bcefec474046c7b55467e8f902
Brunolivier <bpereiraoliveira@gmail.com>
2021-02-09 23:11:37 -0300

    Organizando

---
+++ src/Exercicios.java
@@ -0,0 +1,21 @@
+public class Exercicios {
+
+    public static void main(String[] args) {
+
+        // imprimir o resultado da multiplicacao entre dois numeros
+        int numero1 = 2;
+        int numero2 = 4;
+        System.out.println("O resultado da multiplicação entre " + numero1 + " e " + numero2 + " é: " + (numero1 * numero2));
+
+        //imprimir o resto da divisão entre dois numeros
+        int numero3 = 12;
+        int numero4 = 3;
+        System.out.println("O resto da divisão entre " + numero3 + " e " + numero4 + " é: " + (numero3 % numero4));
+
+        //imprimir o resultado da comparação entre dois numeros
+        int numero5 = 6;
+        int numero6 = 8;
+        System.out.println(numero5 + " é maior que " + numero6 + " ? " + (numero5 > numero6));
+
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,24 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        // imprimir o resultado da multiplicacao entre dois numeros
-        int numero1 = 2;
-        int numero2 = 4;
-        System.out.println(numero1 * numero2);
-
-        //imprimir o resto da divisão entre dois numeros
-        int numero3 = 12;
-        int numero4 = 3;
-        System.out.println(numero3 % numero4);
-
-        //imprimir o resultado da comparação entre dois numeros
-        int numero5 = 6;
-        int numero6 = 8;
-        System.out.println(numero5 + " é maior que " + numero6 + " ? " + (numero5 > numero6));
-
-
-
-
+        System.out.println("Olá mundo! A maldição está quebrada");
 
     }
 
