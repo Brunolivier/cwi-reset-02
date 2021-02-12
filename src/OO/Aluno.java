8a0b6d6d092b878696441387dbfdbaa9157db1a8
Brunolivier <bpereiraoliveira@gmail.com>
2021-02-12 18:27:14 -0300

    colocando alguns comentarios

--- src/OO/Aluno.java
+++ src/OO/Aluno.java
@@ -6,16 +6,19 @@ public class Aluno {
     public int nota;
     public boolean situacaoAprovacao;
 
+    // metodo que recebe nome e nota
     public Aluno(String nome, int nota)
     {
         this.nome = nome;
         this.nota = nota;
+        // chama o metodo de verificar a nota do aluno aprovado
         this.verifica();
         System.out.println("O aluno " + nome + " foi instanciado com a nota " + nota);
         System.out.println("o aluno está aprovado? " + situacaoAprovacao);
 
     }
 
+    // metodo para verificar a nota do aluno se esta aprovado
     public void verifica()
     {
         if (nota >= 7)
