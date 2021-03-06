89094b16ca384d00a81ca65eefe5beed9f9bd0a1
Brunolivier <bpereiraoliveira@gmail.com>
2021-03-06 14:41:47 -0300

    aula 05 exercicios

---
+++ src/exemplo-reset/src/main/java/br/com/cwi/reset/exemploreset/UsuarioController.java
@@ -0,0 +1,15 @@
+package br.com.cwi.reset.exemploreset;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @GetMapping
+    public List<Usuario> listarUsuarios(){
+
+        private UsuarioService usuarioService
+    }
+
+}
